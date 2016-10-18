04cbddf5a0046ae05524ddf12e3ebb3758f42fc2
DiCanio <TwrdikAlex@aol.com>
2016-10-18 16:01:48 +0200

    add a dummy product to the request

--- src/takelist/middleware/product.clj
+++ src/takelist/middleware/product.clj
@@ -1,4 +1,7 @@
 (ns takelist.middleware.product)
 
 (defn wrap-product [handler]
-  handler)
+    (fn [request]
+      (handler (merge request {:product {:id "abc123" :name "Dummy-Product"}}))
+    )
+)
